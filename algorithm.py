860d81f6f4eb77c7c38a087c90be34f419c812f1
Eray-Koc <koceray34@gmail.com>
2024-03-22 18:54:50 +0300

    test

---
+++ algorithm.py
@@ -0,0 +1,97 @@
+#1- İlk iki elemanı 1'e eşit olan, en az 20 elemanlı bir fibonacci serisini liste halinde oluşturan döngü yazalım.
+
+i = 0
+fibonacci = [1 , 1]
+while (len(fibonacci) != 20):
+    value = fibonacci[i] + fibonacci[i + 1]
+    fibonacci.append(value)
+    i += 1
+for items in fibonacci:
+    print(items)
+
+
+#2- Kullanıcıdan aldığı sayının mükemmel olup olmadığını söyleyen bir program yazınız.(Arş. Mükemmel sayı?)
+def greatnum(number):
+    x = 0
+    i = 1
+    while(i < number):
+        if (number % i == 0):
+            x += i
+        i += 1
+    if (number == x):
+        return True
+    else:
+        return False
+  
+        
+#3- Kullanıcıdan girilen sayının EBOB ve EKOK'unu bulan programı yazınız.
+
+def lcm(number, number2):
+    number_array = []
+    x = 1
+    i = 2
+    while number > 1 or number2 > 1:
+        while isprime(i) and (number % i == 0 or number2 % i == 0):
+            if number % i == 0 and number2 % i == 0:
+                number_array.append(i)
+                number = number // i
+                number2 = number2 // i
+            elif number % i == 0:
+                number_array.append(i)
+                number = number // i
+            elif number2 % i == 0:
+                number_array.append(i)
+                number2 = number2 // i
+        i += 1
+    for i in number_array:
+        x *= i
+    return x
+
+def gcd(number, number2):
+    number_array = []
+    x = 1
+    i = 2
+    while number > 1 or number2 > 1:
+        while isprime(i) and (number % i == 0 or number2 % i == 0):
+            if number % i == 0 and number2 % i == 0:
+                number_array.append(i)
+                number = number // i
+                number2 = number2 // i
+            elif number % i == 0:
+                number = number // i
+            elif number2 % i == 0:
+                number2 = number2 // i
+        i += 1
+    for i in number_array:
+        x *= i
+    return x
+
+
+
+
+
+#4- Kullanıcıdan girilen sayının asal sayı olup olmadığını söyleyen bir program yazınız.
+
+
+def isprime(number):
+    i = 2
+    if number == 2:
+        return True
+    while i < number:
+        if (number % i == 0):
+            return False
+        i+=1
+    return True
+
+
+#5- Kullanıcıdan girilen sayının asal çarpanlarını bulan bir program yazınız.
+
+def prime_list(number):
+    number_array = []
+    i = 2
+    while number >= 2:
+        while isprime(i) and number % i == 0:
+            number_array.append(i)
+            number = number / i
+        i+=1
+    return number_array
---
+++ fiveq.py
@@ -0,0 +1,42 @@
+#1-Kullanıcının girdiği boy ve ağırlık değerlerine göre vücut kitle indeksini (VKİ = ağırlık/(boy*boy)) hesaplayınız.
+#   Height = float(input("Boyunuzu giriniz (x.xx): "))
+#   Weight = float(input("Kilonuzu giriniz: "))
+#   Result = Weight / (Height * Height)
+#   print("Vücut kitle indeksiniz: "+ str(Result))
+
+
+#2-Maaşı ve zam oranı girilen işçinin zamlı maaşını hesaplayarak ekranda gösteriniz.
+#   Salary = float(input("Maaşınızı giriniz: "))
+#   IncreaseRate = float(input("Zam oranınızı giriniz: "))
+#   NewSalary = Salary + (Salary * (IncreaseRate / 100))
+#   print("Yeni maaşınız: " + str(NewSalary))
+
+
+#3-Kullanıcının girdiği üç sayı arasında en büyük olanı bulan ve sonucu yazdıran bir program yazınız.
+#   fnumber = int(input("Birinci sayı: "))
+#   snumber = int(input("İkinci sayı: "))
+#   tnumber = int(input("Üçüncü sayı: "))
+#   if fnumber > snumber and fnumber > tnumber:
+#       print("En büyük sayı " + str(fnumber))
+#   elif snumber > fnumber and snumber > tnumber:
+#       print("En büyük sayı " + str(snumber))
+#   else:
+#       print("En büyük sayı " + str(tnumber))
+
+
+#4-Dairenin alanını ve çevresini hesaplayan python kodunu yazınız.(Dairenin yarıçapını kullanıcıdan alınız)
+#   r = float(input("Yarıçap giriniz: "))
+#   pi_num = 3.14
+#   circumference = 2 * pi_num * r
+#   area = 2 * pi_num * (r * r)
+#   print(f"Alan: {area}")
+#   print(f"Çevre: {circumference}")
+
+#5-Kullanıcıdan alınan bir sayının palindrom olup olmadığını bulan bir program yazınız.
+#   number = input("Bir sayı giriniz: ")
+#   number_reverse = number[::-1]
+#   if number_reverse == number:
+#       print("Sayınız palindromdur")
+#   else:
+#       print("Palindrom değildir")
+    
